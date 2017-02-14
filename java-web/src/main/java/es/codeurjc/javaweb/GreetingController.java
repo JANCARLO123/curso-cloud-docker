d5f7e84552dc610742486456d1654e93adcacbe4
Micael Gallego <micael.gallego@gmail.com>
2017-02-14 14:42:48 +0100

    Add javaweb sample project

---
+++ java-web/src/main/java/es/codeurjc/javaweb/GreetingController.java
@@ -0,0 +1,17 @@
+package es.codeurjc.javaweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+	@RequestMapping("/greeting")
+	public String greeting(Model model) {
+		
+		model.addAttribute("name", "World");
+		
+		return "greeting_template";
+	}
+}
